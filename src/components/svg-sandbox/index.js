2c12e3c38f0b154685a97114f53bca767ba516db
Giancarlo Vera <gianverab@gmail.com>
2020-08-05 01:17:43 -0500

    add xAxis and yAxis to the chart

--- src/components/svg-sandbox/index.js
+++ src/components/svg-sandbox/index.js
@@ -9,14 +9,63 @@ const Sandbox = () => {
   const showSvg = () => {
     const width = 600;
     const height = 400;
+    const margin = {
+      top: 20,
+      bottom: 20,
+      left: 30,
+      right: 30,
+    };
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const xVals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const yVals = [5, 6, 2, 7, 9, 1, 3, 4, 8, 3, 6, 7];
 
     const svg = d3
       .select(canvas.current)
       .append("svg")
-      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr(
+        "viewBox",
+        `0 0 ${width + margin.left + margin.right} ${
+          height + margin.top + margin.bottom
+        }`
+      )
       .attr("width", `${width}`)
       .attr("height", `${height}`)
-      .style("background-color", "#fca311");
+      .attr("preserveAspectRatio", "xMinYMin")
+      .style("background-color", "#ffcad4");
+
+    const xScale = d3
+      .scaleBand()
+      .domain(xVals)
+      .range([margin.left, width + margin.left]);
+    const yScale = d3
+      .scaleBand()
+      .domain(yVals)
+      .range([height + margin.bottom, margin.top]);
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg
+      .append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0, ${height + margin.top})`);
+    svg
+      .append("g")
+      .call(yAxis)
+      .attr("transform", `translate(${margin.left}, 0)`);
   };
 
   useEffect(() => {
@@ -36,8 +85,8 @@ const SandboxWrapper = styled.div`
 
 const Canvas = styled.div`
   margin: 0 auto;
-  width: 800px;
-  padding: 0 100px;
+  width: 640px;
+  padding: 0 20px;
   box-sizing: border-box;
 `;
 
--- src/pages/barchart/index.js
+++ src/pages/barchart/index.js
@@ -3,22 +3,6 @@ import Header from "../../components/header";
 import Sandbox from "../../components/svg-sandbox";
 
 const BarChart = () => {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const yvals = [5, 6, 2, 7, 9, 1, 3, 4, 8, 3, 6, 7];
-
   return (
     <div className="barchart">
       <Header />
